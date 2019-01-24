a2ec5be0f7a91a3ce83086afeea0a8b8a3789522
Diana Utan <dia_utzi@yahoo.com>
2019-01-24 21:56:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,47 +1,25 @@
- var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var candyString = "foo"
  
- function addElementToBeginningOfArray(array, element) {
-   return [element, ...array];
-
- }
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
  
- function destructivelyAddElementToBeginningOfArray(array, element) {
-     return array.unshift(element);
-    
-   }
-   
-   function addElementToEndOfArray(array, element) {
-     return [...array, element];
-    
-   }
-   
-   function destructivelyAddElementToEndOfArray(array, element) {
-     
-      return array.push(element);
-     
-   }
-   
-   function accessElementInArray( array, index) {
-     return array[index];
-   }
-   
-   function destructivelyRemoveElementFromBeginnigOfArray(array) {
-     array.shift();
-     return array;
-     }
-   
-   function removeElementFromBeginningOfArray(array) {
-     return array.slice(1);
-   }
-     
-   function destructivelyAddElementToEndOfArray(array)  {
-     array.pop();
-     return array;
-   }
-   
-   function removeElementFromEndOfArray(array) {
-     return array.slice(0, array.length -1);
-     
-   }
-   
-   
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
