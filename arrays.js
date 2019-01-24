787f8af3d8188da8187e28303f78c7bc719b8412
Diana Utan <dia_utzi@yahoo.com>
2019-01-24 22:01:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(chocolateBars, candy) {
   return [candy, ...chocolateBars];
 }
+
  
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candy)
 {
